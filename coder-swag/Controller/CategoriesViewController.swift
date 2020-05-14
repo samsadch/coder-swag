08c27ceff867988b4e26ddd7afddc79539ee08ff
samsadch <samsadch@gmail.com>
2020-05-14 16:11:20 +0400

    Table view added

--- coder-swag/Controller/CategoriesViewController.swift
+++ coder-swag/Controller/CategoriesViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class CategoriesViewController: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Samsad CV on 14/05/20.
+//  Copyright © 2020 Samsad CV. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
